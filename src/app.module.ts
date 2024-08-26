e73e451b2392cdb41d9e1d85d3dc8f2c54cc8e9e
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-08-26 14:41:27 -0300

    feat: add validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { EventsModule } from './events/events.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    DatabaseModule,
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      errorHttpStatusCode: 422,
+    }),
+  );
+
   await app.listen(3000);
+  Logger.log(
+    `Application is running on: ${await app.getUrl()}`,
+    'NestApplication',
+  );
 }
 bootstrap();
