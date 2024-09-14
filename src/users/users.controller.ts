35b1e9accb35fe0f1ce97828318515684c3b672f
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-09-14 18:12:50 -0300

    feat: add id in jwt payload

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,13 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Param,
-  Put,
-  Delete,
-  UseGuards,
   HttpCode,
   HttpStatus,
   UnauthorizedException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { AuthGuard } from 'src/auth/auth.guard';
 import { SignInDto } from 'src/auth/dtos/sign-in.dto';
 import { AuthService } from 'src/auth/auth.service';
 
@@ -24,30 +18,9 @@ export class UsersController {
     private readonly authService: AuthService,
   ) {}
 
-  @Post()
+  @Post('sign-up')
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
-  }
-
-  @UseGuards(AuthGuard)
-  @Get()
-  async findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
-  }
-
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: any) {
-    return this.usersService.update(id, updateUserDto);
-  }
-
-  @Delete(':id')
-  async delete(@Param('id') id: string) {
-    return this.usersService.delete(id);
   }
 
   @HttpCode(HttpStatus.OK)
@@ -57,7 +30,7 @@ export class UsersController {
     if (user?.password !== password) {
       throw new UnauthorizedException();
     }
-    const token = await this.authService.signIn(email, password);
+    const token = await this.authService.signIn(user.id, password);
 
     return {
       token,
